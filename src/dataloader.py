b4cc9a73830eb368f707489244342d111f9189fc
soumodip615c <soumodip615c@gmail.com>
2026-02-01 01:40:57 +0530

    DATA uploading

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 data=pd.read_csv("C:\\Users\\SOUMODIP\\Downloads\\archive (1)\\TCS Historical Data.csv")
 #print(data.head())
 data = data.drop(columns=["Unnamed: 0"])
